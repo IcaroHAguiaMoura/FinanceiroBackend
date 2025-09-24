7b70499d7e591bcc47cb782de4ccfd5ab17e9f31
lucera1 <guilhermelucera0@gmail.com>
2025-09-24 14:57:12 -0300

    Pequeno update em SecurityConfig

--- src/main/java/com/financeiro/backend/config/SecurityConfig.java
+++ src/main/java/com/financeiro/backend/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authotization", "Content-Type"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
